cc4079a13cee79bf805833f061959167c5eb86ce
cheykrampitz <42361477+cheykrampitz@users.noreply.github.com>
2018-08-21 16:26:26 -0600

    Add files via upload

---
+++ Lesson 2.py
@@ -0,0 +1,50 @@
+
+# coding: utf-8
+
+# # Lesson 2 Notebook 
+# by Cheyenne Krampitz 
+
+# ###### 1) Define data science. How do you think you will use Data Science in your career (either as a student or in the Air Force)?
+# 
+# Data science is mathematical and computational data or knowledge. I think it is a key skill in problem solving. In the Air Force, it can be used to solve problems logically and quantitatively. 
+
+# ###### 2) What are the three focus areas in this course?
+# 
+# Exploration, Inference, Prediction
+
+# ###### 3) Install the datascience package (no Jupyter notebook input required).
+# 
+# ###### 4) Do at least 3 basic calculations in three separate boxes.
+
+# In[2]:
+
+
+4 + 4
+
+
+# In[3]:
+
+
+87 * 42
+
+
+# In[4]:
+
+
+6072 / 97
+
+
+# ###### 5) In another box, use python code to print your name followed by a “thumbs up” sign (you may need to do an internet search for Unicode thumbs up character).
+
+# In[49]:
+
+
+print("Cheyenne 👍")
+
+
+# ###### 6) I read once that violent crime is significantly associated with ice cream sales. That is, as ice cream sales increase, so does violent crime. Do high ice cream sales cause an increase in violent crime? Explain this relationship.
+# 
+# Ice cream sales do not cause an increase in crime. The lurking variable in this situation would be summertime. Both events occur in the same season (Summer) and are not directly associated. 
+
+# ###### 7) What is the difference between an observational study and an experiment?
+# An observational study deals with only what you see. For instance, someone would be able to notice an increase in ice cream sales and an increase in crime rates a the same time - this would be an observational study. An experimental study involves testing a theory by manipulating the a variable and keeping the others constant. 
